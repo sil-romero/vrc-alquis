f032f56ec96084836727e2573031d05717d45514
sil-romero <50669265+sil-romero@users.noreply.github.com>
2019-12-11 13:00:31 -0300

    :B

--- ajax/listar_publicaciones_inmuebles.php
+++ ajax/listar_publicaciones_inmuebles.php
@@ -39,7 +39,7 @@ nro_habitaciones, dimension,fecha_hora, id_publicacion, acepta_mascota,nombre_lo
 from inmueble,direccion,imagen,publicacion,localidad,provincia,barrio,tipo_calle,lista_inmueble,subcategoria,categoria 
 where id_barrio=fk_barrio and id_localidad=fk_localidad and id_tipo_calle=fk_tipo_calle and id_imagen=fk_imagen and 
 id_direccion=fk_domicilio and id_categoria=fk_categoria and id_subcategoria=fk_subcategoria and 
-id_lista_inmueble=fk_lista_inmueble and id_inmueble=fk_inmueble And $filtro ORDER BY id_publicacion DESC LIMIT 0,3";
+id_lista_inmueble=fk_lista_inmueble and id_inmueble=fk_inmueble And $filtro ORDER BY id_publicacion DESC LIMIT 0,9";
 
 $result = $pdo->query($listar_publicaciones)->fetchAll();
 /* fetchAll() devuelve un array que contiene todas las filas restantes del conjunto de resultados */
